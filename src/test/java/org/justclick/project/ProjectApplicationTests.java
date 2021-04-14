955e982d65733ffe30bcb8e00c5c1d743c73e2f2
Pedro Mendes <pedro.trabalho.uma@gmail.com>
2021-04-14 20:52:32 +0100

    base setup

---
+++ src/test/java/org/justclick/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.justclick.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
